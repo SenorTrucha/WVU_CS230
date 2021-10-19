ad6972461d4141b79098d98b8870ed1d9e44e06d
SenorTrucha <tjt0012@mix.wvu.edu>
2021-10-19 13:18:47 -0400

    Assignment 7

--- src/app/body/movies/movies.component.ts
+++ src/app/body/movies/movies.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { mock_moviecards } from '../movie-cards/mock-moviecards';
+//import { mock_moviecards } from '../movie-cards/mock-moviecards';
+import { MovieCardService } from '../movie-cards/movie-cards.service';
 import { MovieCard } from '../movie-cards/moviecards.model';
+import { MovieCardInfo } from '../movie-cards/movie-card-info.model';
 
 @Component({
   selector: 'disney-movies',
   templateUrl: './movies.component.html',
 })
-export class MoviesComponent implements OnInit {
-  moviecards: MovieCard [] = [];
+export class MoviesComponent{
+  moviecards: MovieCard[] = [];
 
-  constructor() {
-    for(var moviecard of mock_moviecards){
-      this.moviecards.push(new MovieCard(moviecard))
-    }
+  constructor(private moviecardService: MovieCardService) {
+
+    this.moviecardService.getCardInfo().subscribe((data: MovieCardInfo[]) => {
+
+      for(let i = 0; i < data.length; i++){
+        var moviecard:(MovieCardInfo | undefined) = data[i];
+        this.moviecards.push(new MovieCard(moviecard))
+      }
+
+    })
+    
    }
-    ngOnInit(): void {
-    }
 }
